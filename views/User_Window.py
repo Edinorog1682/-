e2c00eb589b1cc495342e8dabe5dd1b1e04a3cae
Алексей <prom-174@inbox.ru>
2024-03-11 23:36:54 +0500

    mem

--- views/User_Window.py
+++ views/User_Window.py
@@ -1,4 +1,5 @@
-from flet import *
+import flet as ft
+from flet import NavigationDrawer
 from core.style import *
 from core.dicitionary_ru import *
 import time
@@ -8,18 +9,59 @@ from views.login import Login
 class User_Window(Login):
     def __init__(self, page: Page):
         super().__init__(page)
-        self.b=None
-        print(self.b)
-        self.login = self.page.views[0].controls[0].content.controls[2].controls[1].content.controls[0].controls[0].value
-        print(self.page.views[0].controls[0].content)
+        self.i = 0
+        self.b = False
+        self.login_page = None
+        self.login_page = self.page.views[0].controls[0].login_username.value
+        self.my_type_writter_text = Text(self.login_page, no_wrap=False)
+        self.Text = Text(f'Добро пожаловать, {self.login_page}', size=24, color='#f7f7f7',opacity=self.i)
 
-        self.Text = Text(f'Добро пожаловать, {self.login}')
+        self.content = Stack([
+            Container(
+                Column([
+                    Row([self.Text], alignment=MainAxisAlignment.CENTER)
+                ]),  on_hover =  self.glav_anim, margin=ft.margin.symmetric(vertical=page.height / 2)
+            )
 
-        self.content = Container(
-            Stack([self.Text
+        ],visible=True)
+        self.menu_but = ft.ElevatedButton("Show drawer", on_click=self.show_drawer)
+        page.drawer = ft.NavigationDrawer(visible=True,
+                                        controls=[
+                                            ft.Container(height=12),
+                                            ft.NavigationDrawerDestination(
+                                                label="Item 1",
+                                                icon=ft.icons.DOOR_BACK_DOOR_OUTLINED,
+                                                selected_icon_content=ft.Icon(ft.icons.DOOR_BACK_DOOR),
+                                            ),
+                                            ft.Divider(thickness=2),
+                                            ft.NavigationDrawerDestination(
+                                                icon_content=ft.Icon(ft.icons.MAIL_OUTLINED),
+                                                label="Item 2",
+                                                selected_icon=ft.icons.MAIL,
+                                            ),
+                                            ft.NavigationDrawerDestination(
+                                                icon_content=ft.Icon(ft.icons.PHONE_OUTLINED),
+                                                label="Item 3",
+                                                selected_icon=ft.icons.PHONE,
+                                            ),
+                                        ],
+                                        )
+        self.content = ft.Stack([
+            page.drawer, self.menu_but
+        ])#,visible=False
 
-                   ])
-        )
-
-
+    def show_drawer(self, e):
+        self.menu.visible = True
+        self.page.update()
+        self.menu.update()
+    def glav_anim(self,e):
+        self.i = 0
+        if not self.b:
+            while self.i < 1:
+                self.i += 0.001
+                self.Text.opacity = self.i
+                self.page.update()
+                self.b = True
+                time.sleep(0.01)
+                print(self.i)
 
